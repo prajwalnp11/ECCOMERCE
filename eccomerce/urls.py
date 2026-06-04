c6d966417cbaa9e0f649f1e4db86f1b5bc7b8aef
prajwalnp11 <prajwalnp9999@gmail.com>
2026-06-04 22:47:05 +0530

    images accesed to store page

---
+++ eccomerce/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for eccomerce project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from store.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('store/', store, name="store"),
+    path('', main, name="main"),
+    path('cart/', cart, name="cart"),
+    path('checkout/', checkout, name="checkout"),
+
+]
